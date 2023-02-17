25b7301e2fb36665b27031ab53fa97de5d5a7935
derek-le1105 <derekle1105@gmail.com>
2023-02-17 11:38:57 -0800

    Allows users to select from reason list

--- frontend/src/hooks/useStudent.js
+++ frontend/src/hooks/useStudent.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 export const useStudent = () => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
+  const [exists, setExists] = useState(null);
+  const [name, setName] = useState(null);
 
   const student = async (id) => {
     setIsLoading(true);
@@ -16,15 +18,18 @@ export const useStudent = () => {
     });
 
     const json = await response.json();
-
+    console.log(json[0]);
     if (!response.ok) {
       setIsLoading(false);
       setError(json.error);
+      setExists(false);
     }
     if (response.ok) {
       setIsLoading(true);
+      setExists(true);
+      setName(json[0].name.split(" ")[0]);
     }
   };
 
-  return { student, isLoading, error };
+  return { student, isLoading, error, exists, name };
 };
--- frontend/src/pages/StudentForm.js
+++ frontend/src/pages/StudentForm.js
@@ -1,30 +1,77 @@
 import { useState } from "react";
 
 import { useStudent } from "../hooks/useStudent";
+import { useEntry } from "../hooks/useEntry";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import FloatingLabel from "react-bootstrap//FloatingLabel";
 
 const StudentForm = () => {
   const [id, setID] = useState("");
-  const { student, isLoading, error } = useStudent();
+  const { student, isLoading, error, exists, name } = useStudent();
+  const { idInputted, setIDInputted } = useState(false);
+
+  const reasonList = [
+    "Homework/Study",
+    "Office Hours",
+    "Tutoring",
+    "Counseling",
+    "Other",
+  ];
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(id);
     await student(id);
+    setIDInputted(true);
+  };
+
+  const entrySubmit = async (e) => {
+    e.preventDefault();
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3">
-        <Form.Label>ID Number</Form.Label>
-        <Form.Control type="text" onChange={(e) => setID(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <>
+      {exists === null && (
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3">
+            <FloatingLabel label="ID Number" className="mb-3">
+              <Form.Control
+                type="text"
+                onChange={(e) => setID(e.target.value)}
+                placeholder="ID Number"
+              />
+            </FloatingLabel>
+          </Form.Group>
+          <Button variant="primary" type="submit" disabled={isLoading}>
+            Submit
+          </Button>
+        </Form>
+      )}
+      {exists === true && (
+        <Form onSubmit={entrySubmit}>
+          <Form.Group className="mb-3">
+            <h2>{`Welcome ${name}`}</h2>
+            <FloatingLabel label="Reason for coming here:" className="mb-3">
+              <Form.Select aria-label="Default select example">
+                {reasonList.map((reason, index) => {
+                  return (
+                    <option value={reason} key={index}>
+                      {reason}
+                    </option>
+                  );
+                })}
+              </Form.Select>
+            </FloatingLabel>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form.Group>
+        </Form>
+      )}
+      {exists === false && <span>asdfaaf</span>}
+    </>
   );
 };
 
