2654d7b44ff2aa9cdf5006e055630bdf5a8d841d
derek-le1105 <derekle1105@gmail.com>
2023-02-18 13:33:34 -0800

    Added routing for admin login and dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import StudentForm from "./pages/StudentForm";
+import AdminLogin from "./pages/AdminLogin";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="login-container">
-        <StudentForm />
-      </div>
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<StudentForm />} />
+            <Route path="/login" element={<AdminLogin />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
