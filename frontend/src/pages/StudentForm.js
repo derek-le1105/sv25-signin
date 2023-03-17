bba26ad3c89a9969872a654146f1b623dd846f89
derek-le1105 <derekle1105@gmail.com>
2023-03-16 18:24:41 -0700

    Cleaned up unnecessary code

--- frontend/src/pages/StudentForm.js
+++ frontend/src/pages/StudentForm.js
@@ -1,14 +1,7 @@
-//TODO: MAKE BUTTON FOR ADMIN LOGIN
-
 import { useState, useEffect } from "react";
 import { useStudent } from "../hooks/useStudent";
-
 import { useNavigate } from "react-router-dom";
-
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import FloatingLabel from "react-bootstrap/FloatingLabel";
-import Image from "react-bootstrap/Image";
+import { Form, Button, FloatingLabel, Image } from "react-bootstrap";
 
 import EntryForm from "./EntryForm";
 import SignupForm from "./SignupForm";
@@ -16,7 +9,6 @@ import SignupForm from "./SignupForm";
 const StudentForm = () => {
   const [exists, setExists] = useState(null);
   const [id, setID] = useState(null);
-  const [response, setResponse] = useState(false);
   const navigate = useNavigate();
 
   const { getStudent, isLoading, error, name, professors } = useStudent({
@@ -35,65 +27,72 @@ const StudentForm = () => {
     await getStudent(id);
   };
 
-  const adminButton = async (e) => {
-    console.log("admin");
-    //disable current and go to login
+  const adminButton = (e) => {
     navigate("/login");
   };
 
   return (
     <div className="pages">
       <div className="login">
-        {exists === null && (
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3">
-              <Image
-                src="https://pasadena.edu/academics/support/success-centers/stem-centers/images/success-center_STEM.png"
-                rounded
-                fluid
-                className="mb-3"
-              />
-              <FloatingLabel label="ID Number" className="mb-3">
-                <Form.Control
-                  type="number"
-                  onChange={(e) => setID(e.target.value)}
-                  placeholder="ID Number"
-                  value={id === null ? "" : id}
+        {
+          /*for initial display and when user inputs empty*/
+          exists === null && (
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3">
+                <Image
+                  src="https://pasadena.edu/academics/support/success-centers/stem-centers/images/success-center_STEM.png"
+                  rounded
+                  fluid
+                  className="mb-3"
                 />
-                <Form.Text muted>{error}</Form.Text>
-              </FloatingLabel>
-            </Form.Group>
+                <FloatingLabel label="ID Number" className="mb-3">
+                  <Form.Control
+                    type="number"
+                    onChange={(e) => setID(e.target.value)}
+                    placeholder="ID Number"
+                    value={id === null ? "" : id}
+                  />
+                  <Form.Text muted>{error}</Form.Text>
+                </FloatingLabel>
+              </Form.Group>
 
-            <Button variant="primary" type="submit" disabled={isLoading}>
-              Submit
-            </Button>
-            <Button
-              variant="secondary"
-              onClick={adminButton}
-              disabled={isLoading}
-            >
-              Admin
-            </Button>
-          </Form>
-        )}
-        {exists === true && (
-          <EntryForm
-            id={id}
-            name={name}
-            reasonList={reasonList}
-            professors={professors}
-            setID={setID}
-            setExists={setExists}
-          />
-        )}
-        {exists === false && (
-          <SignupForm
-            id={id}
-            reasonList={reasonList}
-            setID={setID}
-            setExists={setExists}
-          />
-        )}
+              <Button variant="primary" type="submit" disabled={isLoading}>
+                Submit
+              </Button>
+              <Button
+                variant="secondary"
+                onClick={adminButton}
+                disabled={isLoading}
+              >
+                Admin
+              </Button>
+            </Form>
+          )
+        }
+        {
+          /*when a student is found in database*/
+          exists === true && (
+            <EntryForm
+              id={id}
+              name={name}
+              reasonList={reasonList}
+              professors={professors}
+              setID={setID}
+              setExists={setExists}
+            />
+          )
+        }
+        {
+          /*when a student isn't found in database*/
+          exists === false && (
+            <SignupForm
+              id={id}
+              reasonList={reasonList}
+              setID={setID}
+              setExists={setExists}
+            />
+          )
+        }
       </div>
     </div>
   );
